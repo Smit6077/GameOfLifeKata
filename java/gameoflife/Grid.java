569ce9b7012938487a47735e64b9dfdd00540147
Smit6077 <61833934+Smit6077@users.noreply.github.com>
2021-04-05 17:33:45 +0200

    Update Grid.java

--- java/gameoflife/Grid.java
+++ java/gameoflife/Grid.java
@@ -50,173 +50,173 @@ public class Grid {
 
     public void generateNextState() {
 
-        for(i=0;i<sizeGrid;i++){
-            for(j=0;j<sizeGrid;j++){
+        for (i = 0; i < sizeGrid; i++) {
+            for (j = 0; j < sizeGrid; j++) {
                 livingNeighbours = 0;
 
                 //Verification de la case en haut a gauche
-                if(i==0 && j==0){
-                    if(verifVoisinDroit(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                if (i == 0 && j == 0) {
+                    if (verifVoisinDroit(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
 
-                    if(verifVoisinDiagDB(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagDB(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinBas(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinBas(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
                     voisinVivant(i, j, livingNeighbours);
                 }
 
                 //Verification des cases au milieu de la premiere ligne
-                if(i==0 && j>0 && j < sizeGrid-1){
-                    if(verifVoisinDroit(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                if (i == 0 && j > 0 && j < sizeGrid - 1) {
+                    if (verifVoisinDroit(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagDB(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagDB(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinBas(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinBas(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinGauche(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinGauche(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagGB(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagGB(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
 
                     voisinVivant(i, j, livingNeighbours);
                 }
 
                 //Verification de la case en haut a droite
-                if(i==0 && j==sizeGrid-1 ){
-                    if(verifVoisinBas(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                if (i == 0 && j == sizeGrid - 1) {
+                    if (verifVoisinBas(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinGauche(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinGauche(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagGB(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagGB(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
                     voisinVivant(i, j, livingNeighbours);
                 }
 
                 //Verification de la case en bas a droite
-                if(i==sizeGrid-1 && j==sizeGrid-1 ){
-                    if(verifVoisinHaut(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                if (i == sizeGrid - 1 && j == sizeGrid - 1) {
+                    if (verifVoisinHaut(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinGauche(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinGauche(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagGH(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagGH(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
                     voisinVivant(i, j, livingNeighbours);
                 }
 
                 //Verification de la case en bas a gauche
-                if(i==sizeGrid-1 && j==0){
-                    if(verifVoisinDroit(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                if (i == sizeGrid - 1 && j == 0) {
+                    if (verifVoisinDroit(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagDH(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagDH(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinHaut(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinHaut(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
                     voisinVivant(i, j, livingNeighbours);
                 }
 
                 //Verification des cases au milieu derniere ligne
-                if(i==sizeGrid-1 && j>0 && j<sizeGrid-1){
-                    if(verifVoisinDroit(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                if (i == sizeGrid - 1 && j > 0 && j < sizeGrid - 1) {
+                    if (verifVoisinDroit(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinGauche(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinGauche(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagDH(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagDH(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagGH(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagGH(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinHaut(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinHaut(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
                     voisinVivant(i, j, livingNeighbours);
                 }
 
                 //Verification des cases au milieu
-                if(i > 0 && i < sizeGrid-1 && j > 0 && j < sizeGrid-1) {
-                    if(verifVoisinDroit(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                if (i > 0 && i < sizeGrid - 1 && j > 0 && j < sizeGrid - 1) {
+                    if (verifVoisinDroit(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinHaut(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinHaut(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagDB(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagDB(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinBas(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinBas(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinGauche(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinGauche(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagGB(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagGB(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagDH(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagDH(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagGH(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagGH(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
                     voisinVivant(i, j, livingNeighbours);
                 }
 
                 //Verification des cases au milieu de la derniere colonne
-                if(i > 0 && j == sizeGrid-1 && i < sizeGrid-1) {
-                    if(verifVoisinHaut(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                if (i > 0 && j == sizeGrid - 1 && i < sizeGrid - 1) {
+                    if (verifVoisinHaut(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinBas(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinBas(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinGauche(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinGauche(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagGH(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagGH(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagGB(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagGB(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
                     voisinVivant(i, j, livingNeighbours);
                 }
 
                 //Verification des cases au milieu de la premiere colonne
-                if(i > 0 && j == 0 && i < sizeGrid-1) {
-                    if(verifVoisinHaut(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                if (i > 0 && j == 0 && i < sizeGrid - 1) {
+                    if (verifVoisinHaut(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinBas(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinBas(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDroit(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDroit(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagDB(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagDB(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
-                    if(verifVoisinDiagDH(i,j)){
-                        livingNeighbours = livingNeighbours +1;
+                    if (verifVoisinDiagDH(i, j)) {
+                        livingNeighbours = livingNeighbours + 1;
                     }
                     voisinVivant(i, j, livingNeighbours);
                 }
@@ -224,64 +224,64 @@ public class Grid {
         }
     }
 
-    public boolean verifVoisinDroit(int i, int j){
-        if(this.cells[i][j+1].toString().compareToIgnoreCase("X") == 0){
+    public boolean verifVoisinDroit(int i, int j) {
+        if (this.cells[i][j + 1].toString().compareToIgnoreCase("X") == 0) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean verifVoisinGauche(int i, int j){
-        if(this.cells[i][j-1].toString().compareToIgnoreCase("X") == 0){
+    public boolean verifVoisinGauche(int i, int j) {
+        if (this.cells[i][j - 1].toString().compareToIgnoreCase("X") == 0) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean verifVoisinHaut(int i, int j){
-        if(this.cells[i-1][j].toString().compareToIgnoreCase("X") == 0){
+    public boolean verifVoisinHaut(int i, int j) {
+        if (this.cells[i - 1][j].toString().compareToIgnoreCase("X") == 0) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean verifVoisinBas(int i, int j){
-        if(this.cells[i+1][j].toString().compareToIgnoreCase("X") == 0){
+    public boolean verifVoisinBas(int i, int j) {
+        if (this.cells[i + 1][j].toString().compareToIgnoreCase("X") == 0) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean verifVoisinDiagGH(int i, int j){
-        if(this.cells[i-1][j-1].toString().compareToIgnoreCase("X") == 0){
+    public boolean verifVoisinDiagGH(int i, int j) {
+        if (this.cells[i - 1][j - 1].toString().compareToIgnoreCase("X") == 0) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean verifVoisinDiagDH(int i, int j){
-        if(this.cells[i-1][j+1].toString().compareToIgnoreCase("X") == 0){
+    public boolean verifVoisinDiagDH(int i, int j) {
+        if (this.cells[i - 1][j + 1].toString().compareToIgnoreCase("X") == 0) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean verifVoisinDiagGB(int i, int j){
-        if(this.cells[i+1][j-1].toString().compareToIgnoreCase("X") == 0){
+    public boolean verifVoisinDiagGB(int i, int j) {
+        if (this.cells[i + 1][j - 1].toString().compareToIgnoreCase("X") == 0) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean verifVoisinDiagDB(int i, int j){
-        if(this.cells[i+1][j+1].toString().compareToIgnoreCase("X") == 0){
+    public boolean verifVoisinDiagDB(int i, int j) {
+        if (this.cells[i + 1][j + 1].toString().compareToIgnoreCase("X") == 0) {
             return true;
         } else {
             return false;
@@ -289,8 +289,8 @@ public class Grid {
     }
 
     public void voisinVivant(int i, int j, int livingNeighbours) {
-        if(this.cells[i][j].isAlive() == true) {
-            if(livingNeighbours == 2 || livingNeighbours == 3) {
+        if (this.cells[i][j].isAlive() == true) {
+            if (livingNeighbours == 2 || livingNeighbours == 3) {
                 this.game[i][j].setIsAlive(true);
             } else {
                 this.game[i][j].setIsAlive(false);
@@ -306,14 +306,14 @@ public class Grid {
 
     public String toString() {
         String tab = "";
-        for(i=0;i<sizeGrid;i++){
-            for(j=0;j<sizeGrid;j++){
+        for (i = 0; i < sizeGrid; i++) {
+            for (j = 0; j < sizeGrid; j++) {
                 tab += this.game[i][j];
-                if(j!=sizeGrid-1){
+                if (j != sizeGrid - 1) {
                     tab += " ";
                 }
             }
-            if(i!=sizeGrid-1){
+            if (i != sizeGrid - 1) {
                 tab += "\n";
             }
         }
